d02a821d7c0bdd1640f9af743cf0b7a17ebe9f7b
Brandon McLean <brandonmclean@live.com>
2015-12-26 10:07:34 -0500

    Updated accordion functionality for expenses for Bootstrap 3

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,12 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!
+  before_action :authorize_user, :only => [:edit, :update, :destroy]
   
   def index
+    puts "In Expense#index method"
   end
   
-  def create
-    
+  def create    
     params = expense_params
     params[:date] = Date.strptime(params[:date],"%Y-%m-%d")
     @expense = current_user.expenses.create(params)
@@ -26,20 +27,22 @@ class ExpensesController < ApplicationController
   end
   
   def update
+    puts "In Expense#update method"
     updated_expense = current_expense
     
-    if !current_expense.valid_expense_change?(expense_params[:amt_charged].to_f)
-      flash[:alert] = "Please delete pending payment first"
-      redirect_to expenses_path and return
-    end
+#    if !current_expense.valid_expense_change?(expense_params[:amt_charged].to_f)
+#      flash[:alert] = "Please delete pending payment first"
+#      redirect_to expenses_path and return
+#    end
       
-    current_expense.update_attributes(expense_params)
+    current_expense.update_attributes(expense_params.merge(:user => current_user))
     
-    if current_expense.valid?
-      redirect_to expenses_path
-    else
-      redirect_to edit_expense_path(updated_expense)
-    end
+#    if current_expense.valid?
+#      redirect_to expenses_path
+#    else
+#      redirect_to edit_expense_path(updated_expense)
+#    end
+    render :index
   end
   
   def destroy
@@ -69,8 +72,15 @@ class ExpensesController < ApplicationController
     @plans ||= current_user.payplans
   end
   
+  def authorize_user
+    if current_user != current_expense.user
+      flash[:alert] = "Unauthorized User!"
+      render :text => "Unauthorized action", :status => :unprocessable_entity
+    end
+  end
+  
   def expense_params
-    params.require('expense').permit(:date, :retailer, :amt_charged, :amt_paid, :split, :how_to_pay, :payment_status, :card_id)
+    params.require('expense').permit(:date, :retailer, :amt_charged, :amt_paid, :split, :how_to_pay, :payment_status, :card_id, "responsible_party_id")
   end
 end
 

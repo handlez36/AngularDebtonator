ee792f0c7d93fea106e8ca5de35f882fcb252067
Brandon McLean <brandonmclean@live.com>
2016-09-11 09:18:04 -0400

    Added expense summary section below graph, due to graph popup text getting cut off a lot

--- app/helpers/payplans_helper.rb
+++ app/helpers/payplans_helper.rb
@@ -1,2 +1,24 @@
 module PayplansHelper
+  def expense_summary(condition)
+    summary_str = "<table class='expense-summary-table'><th>Expense</th><th>Before</th><th>After</th>"
+    
+    if condition == "party"
+      expenses_before_payment = current_user.get_expense_total_by_party
+      expenses_after_payment = current_user.get_expense_total_by_party(false)
+    else
+      expenses_before_payment = current_user.get_expense_total_by_card
+      expenses_after_payment = current_user.get_expense_total_by_card(false)
+    end
+    
+    expenses_before_payment.each do |expense|
+      summary_str += "<tr>"
+      current_party = expense[:label]
+      remaining_expense = expenses_after_payment.select { |e| e[:label] == current_party }.first[:value] || 0
+      summary_str += "<td>#{current_party}</td><td>#{number_to_currency(expense[:value])}</td><td>#{number_to_currency(remaining_expense)}</td>"
+      summary_str += "</tr>"
+    end
+    
+    summary_str += "</table>"
+    summary_str.html_safe
+  end
 end

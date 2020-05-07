3eb6beb2eef6786d63556edc744fe33595809896
Brandon McLean <brandonmclean@live.com>
2020-05-07 00:15:32 -0400

    Fix production compile error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.assets.paths.reject! { |path| ['graphiql/rails/application.js', 'graphiql/rails/application.css'].include? path }
 end
